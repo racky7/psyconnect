e3c753eb9822b38510386b56b446538be3921733
racky7 <raj@tealfeed.com>
2024-05-10 17:31:42 +0530

    feat: added card to schedule a session

--- app/(protected)/user/page.tsx
+++ app/(protected)/user/page.tsx
@@ -13,6 +13,9 @@ export default async function Page() {
   }
   return (
     <div className="w-full px-8 space-y-6">
+      <h2 className="text-3xl font-medium">
+        Welcome to PsyConnect, {session?.user?.name}
+      </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <SessionCard
           upcoming={true}
@@ -24,6 +27,23 @@ export default async function Page() {
       </div>
       <h3 className="text-xl">EXPLORE</h3>
       <div className="grid grid-cols-2 gap-5">
+        <div className="p-4 ring-2 ring-gray-200 shadow-md col-span-1 flex items-center rounded-md">
+          <div className="flex-1">
+            Feeling Overwhelmed? Talk to our Therapists. they are here to help
+            you.
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <Image
+              src={`${CLOUDINARY_BASE_URL}/psyconnect/psyconnect/undraw_group_video_re_btu7_1_sfubzt.svg`}
+              alt=""
+              width={100}
+              height={100}
+            />
+            <Link href="counselors">
+              <Button size="sm">Schedule a Session</Button>
+            </Link>
+          </div>
+        </div>
         <div className="p-4 ring-2 ring-gray-200 shadow-md col-span-1 flex items-center rounded-md">
           <div className="flex-1">
             Your Creative Release-Capture Your thoughts, & let your emotions go
